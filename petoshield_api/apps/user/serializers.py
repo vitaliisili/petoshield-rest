6986632d89fd2f7201ca9f87d12e69ad1a48a12c
Vitalii Sili <vitaliisili@yahoo.com>
2023-10-25 16:10:32 +0200

    🐛 Fix a bug. with ordering a searching fields

--- petoshield_api/apps/user/serializers.py
+++ petoshield_api/apps/user/serializers.py
@@ -5,7 +5,7 @@ from rest_framework.serializers import ModelSerializer
 class UserSerializer(ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ['id', 'email', 'name', 'password']
+        fields = ['id', 'email', 'name']
         extra_kwargs = {'password': {'write_only': True}}
         read_only_fields = ['id']
 
