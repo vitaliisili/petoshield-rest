b24edb9bf77712993ce714d203080304763aaa3d
Vitalii Sili <vitaliisili@yahoo.com>
2023-10-26 22:06:12 +0200

    👔 Add or update business logic Add logic to get serializer in base who did the request

--- petoshield_api/apps/user/views.py
+++ petoshield_api/apps/user/views.py
@@ -1,13 +1,16 @@
 from django.contrib.auth import get_user_model
 from rest_framework.viewsets import ModelViewSet
-
 from apps.user.permissions import UserPermission
-from apps.user.serializers import UserSerializer
+from apps.user.serializers import BaseUserSerializer, ExtendUserSerializer
 
 
 class UserViewSet(ModelViewSet):
-    serializer_class = UserSerializer
     queryset = get_user_model().objects.all()
     permission_classes = (UserPermission,)
     search_fields = ['username', 'email']
     ordering_fields = ['username', 'created_at']
+
+    def get_serializer_class(self):
+        if self.request.user.is_staff:
+            return ExtendUserSerializer
+        return BaseUserSerializer
