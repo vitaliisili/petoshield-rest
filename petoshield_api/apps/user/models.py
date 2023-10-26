5175d7efedf3c9f23aebb61ae0b47d24f860e098
Vitalii Sili <vitaliisili@yahoo.com>
2023-10-26 22:03:25 +0200

    👔 Add or update business logic Add new model class role

--- petoshield_api/apps/user/models.py
+++ petoshield_api/apps/user/models.py
@@ -2,6 +2,11 @@ from django.contrib.auth.models import PermissionsMixin, BaseUserManager, Abstra
 from django.db import models
 
 
+class Role(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
@@ -10,8 +15,7 @@ class UserManager(BaseUserManager):
             raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(using=self._db)
-
+        user.save()
         return user
 
     def create_superuser(self, email, password):
@@ -19,8 +23,8 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save(using=self._db)
-
+        user.role = Role.objects.get(name='admin')
+        user.save()
         return user
 
 
@@ -30,6 +34,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    role = models.ForeignKey(Role, on_delete=models.SET_DEFAULT, default=1, related_name='users')
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
     objects = UserManager()
