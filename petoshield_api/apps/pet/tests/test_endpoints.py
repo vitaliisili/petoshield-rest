177d74c4b71a777ed38fbf010f8c7485c5bb58d9
Irina <ipotereanu@gmail.com>
2023-10-26 23:02:49 +0200

    added tests for pets endpoints and fixed breeds endpoint's tests

--- petoshield_api/apps/pet/tests/test_endpoints.py
+++ petoshield_api/apps/pet/tests/test_endpoints.py
@@ -5,14 +5,16 @@ class TestBreedEndpoints:
     
     endpoint = '/api/pet-profile/breeds/'
     
-    def test_breed_list_admin_access(self, staff_user, api_client):
+    def test_breed_list_admin_access(self, staff_user, api_client, breeds_list):
         api_client.force_authenticate(staff_user)
         response = api_client.get(self.endpoint)
+        assert len(json.loads(response.content)) == len(breeds_list)
         assert response.status_code == 200
         
-    def test_breed_list_user_access(self, simple_user, api_client):
+    def test_breed_list_user_access(self, simple_user, api_client,breeds_list):
         api_client.force_authenticate(simple_user)
         response = api_client.get(self.endpoint)
+        assert len(json.loads(response.content)) == len(breeds_list)
         assert response.status_code == 200
         
     def test_breed_list_with_unauthenticated_user(self, api_client):
@@ -101,3 +103,94 @@ class TestBreedEndpoints:
         api_client.force_authenticate(admin_user)
         response = api_client.delete(f'{self.endpoint}{breed.id}/')
         assert response.status_code == 204
+        
+class TestPetsEndpoints:
+    endpoint = '/api/pet-profile/pets/'
+    
+    def test_pets_list_with_admin_success(self, staff_user,pets_list, api_client):
+        api_client.force_authenticate(staff_user)
+        response = api_client.get(self.endpoint)
+        assert len(json.loads(response.content)) == len(pets_list)
+        assert response.status_code == 200  
+
+    def test_pets_list_with_unauthorized_user(self, simple_user, api_client):
+        api_client.force_authenticate(simple_user)
+        response = api_client.get(self.endpoint)
+        assert response.status_code == 403
+
+    def test_pets_list_with_unauthenticated_user(self, api_client):
+        response = api_client.get(self.endpoint)
+        assert response.status_code == 401
+        
+    def test_get_user_pet_by_id_with_staff_permission(self,staff_user,pet_created_by_user,api_client):
+        api_client.force_authenticate(staff_user)
+        response=api_client.get(f'{self.endpoint}{pet_created_by_user.id}/')
+        assert response.status_code == 200
+        assert json.loads(response.content).get('user') == pet_created_by_user.user.id
+        
+    def test_get_admin_pet_by_id_with_simple_user_not_allowed(self,simple_user,pet_created_by_admin,api_client):
+        api_client.force_authenticate(simple_user)
+        response=api_client.get(f'{self.endpoint}{pet_created_by_admin.id}/')
+        assert response.status_code == 403
+        
+    def test_put_user_pet_with_staff_permissions(self,staff_user,breed,simple_user, pet_created_by_user, api_client):
+        api_client.force_authenticate(staff_user)
+        response = api_client.put(f'{self.endpoint}{pet_created_by_user.id}/',
+                                 {     
+                                    "name":"Simple User dog1",
+                                    "age":6,
+                                    "gender":'M',
+                                    "species":'dog',
+                                    "breed":breed.id,
+                                    "user":simple_user.id
+                                 }
+                              )
+        assert response.status_code == 200
+        assert json.loads(response.content).get('name') == "Simple User dog1"
+        assert json.loads(response.content).get('age') == 6
+        
+    def test_put_admin_pet_with_user_not_allowed(self,staff_user,breed,simple_user, pet_created_by_admin, api_client):
+        api_client.force_authenticate(simple_user)
+        response = api_client.put(f'{self.endpoint}{pet_created_by_admin.id}/',
+                                 {     
+                                    "name":"Admin Doggy dog1",
+                                    "age":6,
+                                    "gender":'M',
+                                    "species":'dog',
+                                    "breed":breed.id,
+                                    "user":staff_user.id
+                                 }
+                              )
+        assert response.status_code == 403
+        
+    def test_patch_user_pet_with_staff_permissions(self,staff_user, pet_created_by_user, api_client):
+        api_client.force_authenticate(staff_user)
+        response = api_client.patch(f'{self.endpoint}{pet_created_by_user.id}/',
+                                 {     
+                                    "name":"Simple User dog1",
+                                 }
+                              )
+        assert response.status_code == 200
+        assert json.loads(response.content).get('name') == "Simple User dog1"
+        
+    def test_patch_admin_pet_with_user_not_allowed(self,simple_user, pet_created_by_admin, api_client):
+        api_client.force_authenticate(simple_user)
+        response = api_client.patch(f'{self.endpoint}{pet_created_by_admin.id}/',
+                                 {     
+                                    "name":"Admin Doggy dog1",
+                                 }
+                              )
+        assert response.status_code == 403
+        
+    def test_delete_user_pet_with_staff_permissions(self,staff_user,pet_created_by_user, api_client):
+        api_client.force_authenticate(staff_user)
+        response = api_client.delete(f'{self.endpoint}{pet_created_by_user.id}/')
+        assert response.status_code == 204
+
+        
+    def test_delete_admin_pet_with_user_not_allowed(self,simple_user, pet_created_by_admin, api_client):
+        api_client.force_authenticate(simple_user)
+        response = api_client.delete(f'{self.endpoint}{pet_created_by_admin.id}/')
+        assert response.status_code == 403
+        
+    
