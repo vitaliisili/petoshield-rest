0c135cc995328f9f284db9a250ee479cc46a1d21
Vitalii Sili <vitaliisili@yahoo.com>
2023-10-27 13:22:07 +0200

    fix test issue with a role model

--- petoshield_api/apps/user/tests/conftest.py
+++ petoshield_api/apps/user/tests/conftest.py
@@ -1,13 +1,24 @@
 import pytest
 from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
+from apps.user.models import Role
+
 
 @pytest.fixture
 def api_client():
     return APIClient()
 
 @pytest.fixture
-def staff_user(db):
+def roles(db):
+    custom_roles = [
+        Role.objects.create(name='client'),
+        Role.objects.create(name='admin'),
+        Role.objects.create(name='provider'),
+    ]
+    return custom_roles
+
+@pytest.fixture
+def staff_user(roles):
     user_staff = get_user_model().objects.create_superuser('admin@mail.com', 'password1A@')
     assert user_staff.email == 'admin@mail.com'
     assert user_staff.is_superuser
@@ -15,9 +26,11 @@ def staff_user(db):
     return user_staff
 
 @pytest.fixture
-def simple_user(db):
-    simple_user = get_user_model().objects.create_user(
-        email='simple_user@mail.com', password='password1A@', name='Simple User')
+def simple_user(roles):
+    simple_user = get_user_model().objects.create_user(email='simple_user@mail.com',
+                                                       password='password1A@',
+                                                       name='Simple User',
+                                                       role=roles[0])
 
     assert simple_user.email == 'simple_user@mail.com'
     assert not simple_user.is_superuser
@@ -26,12 +39,23 @@ def simple_user(db):
 
 
 @pytest.fixture
-def users_list(db, staff_user, simple_user):
+def users_list(roles, staff_user, simple_user):
     users_list = [
         staff_user,
         simple_user,
-        get_user_model().objects.create_user(email='example1@mail.com', password='password1A@', name='Example Name1'),
-        get_user_model().objects.create_user(email='example2@mail.com', password='password1A@', name='Example Name2'),
-        get_user_model().objects.create_user(email='example3@mail.com', password='password1A@', name='Example Name3'),
+        get_user_model().objects.create_user(email='example1@mail.com',
+                                             password='password1A@',
+                                             name='Example Name1',
+                                             role=roles[0]),
+
+        get_user_model().objects.create_user(email='example2@mail.com',
+                                             password='password1A@',
+                                             name='Example Name2',
+                                             role=roles[0]),
+
+        get_user_model().objects.create_user(email='example3@mail.com',
+                                             password='password1A@',
+                                             name='Example Name3',
+                                             role=roles[0]),
     ]
     return users_list
