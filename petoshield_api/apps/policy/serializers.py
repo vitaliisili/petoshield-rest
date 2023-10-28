1bd280ed0a0af5e255fe61d732cae4c1e0cc7507
Vitalii Sili <vitaliisili@yahoo.com>
2023-10-28 22:19:40 +0200

    👔 Add or update business logic, Add new logic for registration provider

--- petoshield_api/apps/policy/serializers.py
+++ petoshield_api/apps/policy/serializers.py
@@ -1,31 +1,43 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from apps.policy.models import ServiceProvider, Policy, InsuranceCase, IncomingInvoice
+from apps.user.serializers import BaseUserSerializer
 
-class ServiceProviderSerializer(ModelSerializer):
-    
+
+class ServiceProviderSerializer(serializers.ModelSerializer):
     class Meta:
         model = ServiceProvider
-        fields = '__all__'
-        read_only_fields = ["id"]
+        exclude = ['user']
+        read_only_fields = ['id', 'created_at', 'updated_at']
 
-class PolicySerializer(ModelSerializer):
-    
+
+class UserServiceProviderSerializer(serializers.Serializer):
+    user = BaseUserSerializer()
+    service_provider = ServiceProviderSerializer()
+
+
+class PolicySerializer(serializers.ModelSerializer):
     class Meta:
         model = Policy
         fields = '__all__'
-        read_only_fields = ["id"]
-        
-class InsuranceCaseSerializer(ModelSerializer):
+        read_only_fields = ['id']
 
+
+class InsuranceCaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = InsuranceCase
         fields = '__all__'
-        read_only_fields = ["id"]
-        
+        read_only_fields = ['id', 'total_cost']
 
-class IncomingInvoiceSerializer(ModelSerializer):
-    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        try:
+            representation['total_cost'] = IncomingInvoice.objects.get(insurance_case=instance).amount
+        except IncomingInvoice.DoesNotExist:  # noqa
+            representation['total_cost'] = None
+        return representation
+
+class IncomingInvoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = IncomingInvoice
         fields = '__all__'
-        read_only_fields = ["id"]
+        read_only_fields = ['id']
