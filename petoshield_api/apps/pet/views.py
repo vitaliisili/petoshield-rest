d17582e9edddd624261b6f52a25a5236009d802a
Vitalii Sili <vitaliisili@yahoo.com>
2023-10-30 22:21:54 +0100

    🚩 Add, update, or remove feature flags. add new action and url endpoint for creating user and pet together

--- petoshield_api/apps/pet/views.py
+++ petoshield_api/apps/pet/views.py
@@ -1,13 +1,16 @@
-from rest_framework import viewsets
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from apps.pet.models import Pet, Breed
 from apps.pet.permissions import IsStaffOrOwner
-from apps.pet.serializers import PetSerializer, BaseBreedSerializer, ExtendBreedSerializer
+from apps.pet.serializers import PetSerializer, BaseBreedSerializer, ExtendBreedSerializer, PetUserCombinedSerializer
+from apps.user.models import Role
 
 
 class PetViewSet(viewsets.ModelViewSet):
     serializer_class = PetSerializer
-    queryset = Pet.objects.all()
     permission_classes = (IsAuthenticated, IsStaffOrOwner)
     search_fields = ['$name']
     ordering_fields = ['created_at', 'name', 'gender', 'species', 'age']
@@ -16,6 +19,26 @@ class PetViewSet(viewsets.ModelViewSet):
         if not request.user.is_staff:
             request.data['user'] = request.user.id
         return super().create(request, *args, **kwargs)
+
+    @action(methods=['post'], detail=False)
+    def create_new_account(self, request, pk=None):
+        serializer = PetUserCombinedSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data['user']
+        user['role'] = Role.objects.get(name='client')
+        user_instance = get_user_model().objects.create_user(**user)
+
+        pet = serializer.validated_data['pet']
+        pet['user'] = user_instance
+        Pet.objects.create(**pet)
+
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+    def get_queryset(self):
+        if not self.request.user.is_staff:
+            return Pet.objects.filter(user=self.request.user)
+        return Pet.objects.all()
 
 
 class BreedViewSet(viewsets.ModelViewSet):
