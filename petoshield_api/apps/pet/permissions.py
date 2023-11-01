07d5668586c7930514f51062438a3cad07466275
Vitalii Sili <vitaliisili@yahoo.com>
2023-11-01 13:56:02 +0100

    🔒️ Fix security issues. Fix breed and pet permission now pet list can request anonymous user

--- petoshield_api/apps/pet/permissions.py
+++ petoshield_api/apps/pet/permissions.py
@@ -1,28 +1,40 @@
 from rest_framework import permissions
 
 
-class IsStaffOrOwner(permissions.BasePermission):
+class BreedPermissions(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if view.action in ['create', 'update', 'partial_update', 'destroy']:
+            return request.user.is_authenticated and request.user.is_staff
+        elif view.action == 'list':
+            return True
+        elif view.action == 'retrieve':
+            return request.user.is_authenticated and request.user.role.name in ['client', 'admin']
+        else:
+            return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_authenticated:
+            return True
+        return False
+
+
+class PetPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        if view.action == 'create':
-            if view.basename == 'breeds':
-                return request.user.is_staff
-            return request.user.role.name in ['client', 'admin']
-        elif view.action in ['list', 'retrieve', 'update', 'partial_update', 'destroy']:
-            return request.user.role.name in ['client', 'admin']
+        if view.action in ['create', 'list', 'retrieve', 'update', 'partial_update', 'destroy']:
+            return request.user.is_authenticated and request.user.role.name in ['client', 'admin']
         elif view.action == 'create_new_account':
             return True
         else:
             return False
 
     def has_object_permission(self, request, view, obj):
+        if not request.user.is_authenticated:
+            return False
+
         if view.action == 'retrieve':
-            if view.basename == 'breeds':
-                return True
             return obj.user == request.user or request.user.is_staff
         elif view.action in ['update', 'partial_update', 'destroy']:
-            if view.basename == 'breeds':
-                return request.user.is_staff
             return obj.user == request.user or request.user.is_staff
         else:
             return False
