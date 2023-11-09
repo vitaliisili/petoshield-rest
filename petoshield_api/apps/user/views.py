67a7e4ec18a9dd64593d123cdbf186fa2622706a
Timur <timur@shakirov.com.ua>
2023-11-09 09:20:58 +0100

    added TODO in view

--- petoshield_api/apps/user/views.py
+++ petoshield_api/apps/user/views.py
@@ -35,7 +35,7 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         
-        
+        #TODO: generate token and save it in the database
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
