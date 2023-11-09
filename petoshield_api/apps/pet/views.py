df43723a0b6d1b9520aa084ecf0bd243f5f5c325
Vitalii Sili <vitaliisili@yahoo.com>
2023-11-09 22:37:46 +0100

    👔 Add or update business logic. Now when pet is created policy will be created to

--- petoshield_api/apps/pet/views.py
+++ petoshield_api/apps/pet/views.py
@@ -1,15 +1,18 @@
+import uuid
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-
 from apps.core.utils import EmailSender
 from apps.pet.models import Pet, Breed
 from apps.pet.permissions import BreedPermissions, PetPermission
 from apps.pet.serializers import PetSerializer, BaseBreedSerializer, ExtendBreedSerializer, PetUserCombinedSerializer
 from apps.pet.filters import BreedFilter, PetFilter
+from apps.policy.models import Policy
 from apps.user.models import Role
-
+from datetime import date
+from apps.policy import utils
+from config import settings
 
 
 class PetViewSet(viewsets.ModelViewSet):
@@ -23,7 +26,25 @@ class PetViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         if not request.user.is_staff:
             request.data['user'] = request.user.id
-        return super().create(request, *args, **kwargs)
+
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        pet_instance = serializer.save()
+
+        Policy.objects.create(
+            policy_number=uuid.uuid4(),
+            start_date=date.today(),
+            end_date=date.today().replace(year=date.today().year + 1),
+            initial_limit=settings.POLICY_INITIAL_LIMIT,
+            current_limit=settings.POLICY_INITIAL_LIMIT,
+            deductible=settings.POLICY_DEDUCTIBLE,
+            status='invalid',
+            pet=pet_instance,
+            price=utils.get_policy_price(pet_instance)
+        )
+
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
     def update(self, request, *args, **kwargs):
         if not request.user.is_staff:
@@ -41,10 +62,21 @@ class PetViewSet(viewsets.ModelViewSet):
 
         pet = serializer.validated_data['pet']
         pet['user'] = user_instance
-        Pet.objects.create(**pet)
+        pet_instance = Pet.objects.create(**pet)
 
-        EmailSender.send_confirmation_email(user, request.META.get('HTTP_REFERER'))
+        Policy.objects.create(
+            policy_number=uuid.uuid4(),
+            start_date=date.today(),
+            end_date=date.today().replace(year=date.today().year + 1),
+            initial_limit=settings.POLICY_INITIAL_LIMIT,
+            current_limit=settings.POLICY_INITIAL_LIMIT,
+            deductible=settings.POLICY_DEDUCTIBLE,
+            status='invalid',
+            pet=pet_instance,
+            price=utils.get_policy_price(pet_instance)
+        )
 
+        EmailSender.send_confirmation_email(user_instance, request.META.get('HTTP_REFERER'))
         return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
     def get_queryset(self):
