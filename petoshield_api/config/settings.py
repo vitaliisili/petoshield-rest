ac280a0f5fc292f8f86dec12e4b3ac334253c0ae
Vitalii Sili <vitaliisili@yahoo.com>
2023-11-13 21:25:26 +0100

    ➕ Add a dependency. Add whiteNoize for serving static files in production

--- petoshield_api/config/settings.py
+++ petoshield_api/config/settings.py
@@ -62,6 +62,7 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,6 +128,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = Path(BASE_DIR, 'static/')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = Path(BASE_DIR, 'media/')
