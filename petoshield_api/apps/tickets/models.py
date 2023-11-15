ac6a781f72683a78a0819ccf4506094eb0a90388
Timur <timur@shakirov.com.ua>
2023-11-15 11:03:29 +0100

    corrected model

--- petoshield_api/apps/tickets/models.py
+++ petoshield_api/apps/tickets/models.py
@@ -13,17 +13,7 @@ class Ticket(BaseModel):
     visitor_name = models.CharField(max_length=125)
     visitor_email = models.EmailField()
     visitor_message = models.TextField(max_length=1000)
-
-    is_client = models.BooleanField(default=False)
     ticket_status = models.CharField(choices=STATUS_CHOICES, default='new')
-
-    def save(self):
-        if User.objects.filter(email=self.visitor_email).exists():
-            self.is_client = True
-        return super().save()
 
     def __str__(self) -> str:
         return f'Ticket|{self.pk}|{self.user_message[:50]}'
-
-    class Meta:
-        verbose_name_plural = 'tickets'
