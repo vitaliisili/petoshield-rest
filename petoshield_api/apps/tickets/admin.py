3fd7611808abe2b0cf0109a90fcdca1ba023463d
Timur <timur@shakirov.com.ua>
2023-11-15 10:32:18 +0100

    added migration file, corrected admin.py

--- petoshield_api/apps/tickets/admin.py
+++ petoshield_api/apps/tickets/admin.py
@@ -6,17 +6,18 @@ class CustomTicket(admin.ModelAdmin):
     list_display = (
         "id",
         "created_at",
+        "visitor_name",
+        "visitor_email",
         "get_description",
         "ticket_status",
-        "staff_user",
     )
 
-    @admin.display(description="visitor_message")
+    @admin.display(description="Visitor Message")
     def get_description(self, obj):
         if len(obj.visitor_message) > 50:
-            return f"{obj.description[:50]}..."
+            return f"{obj.visitor_message[:50]}..."
 
-        return f"{obj.description[:50]}"
+        return f"{obj.visitor_message[:50]}"
 
 
 admin.site.register(models.Ticket, CustomTicket)
