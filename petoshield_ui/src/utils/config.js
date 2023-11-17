991b9c4fd1f80e640e362a065c28eb0049e3875b
Vitalii Sili <vitaliisili@yahoo.com>
2023-11-17 13:57:54 +0100

    🐛 Fix a bug. Fix payment redirect link

--- petoshield_ui/src/utils/config.js
+++ petoshield_ui/src/utils/config.js
@@ -1,2 +1,3 @@
 export const ANNUAL_DISCOUNT = 24
 export const RESET_PASSWORD_REDIRECT_LINK = `${window.location.origin}/pass-recovery/confirm`
+export const PAYMENT_REDIRECT_LINK = `${window.location.origin}/payment`
